d8f1717646b375db06c3ea5082d40939f3d86f0b
Daniel Porter <dpreid@gmail.com>
2022-03-20 17:38:33 +0000

    Move resolver configuration to includes file

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-sed -i -e"s/127.0.0.11/`getent hosts qcacher-dns | awk '{ print $1 }'`/" /etc/nginx/conf.d/01_resolver
+sed -i -e"s/127.0.0.11/`getent hosts qcacher-dns | awk '{ print $1 }'`/" /etc/nginx/includes/resolver
 
 exec nginx -g "daemon off;"

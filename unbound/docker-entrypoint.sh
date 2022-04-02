90207def2896e6a24f485042fa6f9527cf081a14
Daniel Porter <dpreid@gmail.com>
2022-04-02 16:18:13 +0100

    Use A records for DNS resolution
    Unbound isn't authoritative so local resolution is needed.

--- unbound/docker-entrypoint.sh
+++ unbound/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -n ${CACHE_IP+x} ]; then
-    echo "local-data: \"qcacher IN A ${CACHE_IP}\"" >> /etc/unbound/settings.conf
+    echo "${CACHE_IP} qcacher" >> /etc/hosts
 fi
 if [ -n ${CACHE_SUBNET+x} ]; then
     echo "access-control-view: ${CACHE_SUBNET} qcacher" >> /etc/unbound/settings.conf
